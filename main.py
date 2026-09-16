1538c05242680b976af96af6ee057199741d675a
mairon <ragelitogavina@gmail.com>
2026-09-16 17:55:43 -0400

    agregamos opcion de ver catalogo completo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-calalogo_negocio = [
+catalogo_negocio = [
     {"nombre": "masajes corporales", "precio": 500.00, "disponivilidad": True},
     {"nombre": "manicura", "precio": 200.00, "disponivilidad": True},
     {"nombre": "pedicura", "precio": 300.00, "disponivilidad": False}
@@ -8,8 +8,13 @@ calalogo_negocio = [
 
 def main():
     while True:
+        print("1. Ver catálogo completo")
         print("0. Salir")
         opcion = input("Elige una opción: ")
+
+        if opcion == "1":
+            for producto in catalogo_negocio:
+                print(f"{producto['nombre']}: ${producto['precio']}")
 
         if opcion == "0":
             print("Adiós")
